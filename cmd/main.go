677d727bd9cd23bd37394e304ed897841afc556c
akalanitski <akalanitski@gmail.com>
2025-05-12 16:42:09 +0200

    Write simple markdown formatter

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
-	"go-md2tex/pkg/tokenizer"
+	"go-md2tex/pkg/markdown"
 )
 
 func main() {
 	text := `
-// A Go string is a read-only slice of bytes. The language
-// and the standard library treat strings specially - as
-// containers of text encoded in [UTF-8](https://en.wikipedia.org/wiki/UTF-8).
-// In other languages, strings are made of "characters".
-// In Go, the concept of a character is called a rune - it's
-// an integer that represents a Unicode code point.
-// [This Go blog post](https://go.dev/blog/strings) is a good
-// introduction to the topic.`
-	t := tokenizer.New(text)
-	fmt.Printf("TOKENS:\n" + t.String())
+A Go string is a *read-only* slice & of _ bytes. The language
+and the standard library treat strings specially - as
+containers of text encoded in [UTF-8](https://en.wikipedia.org/wiki/UTF-8).
+
+In other languages, strings are made of "characters".
+In Go, the concept of a character is called a rune - it's
+an integer that represents a Unicode code point.
+[This Go blog post](https://go.dev/blog/strings) is a good
+introduction to the topic.`
+
+	fmt.Printf("TOKENS:\n" + markdown.MarkdownToTex(text))
+
 }
